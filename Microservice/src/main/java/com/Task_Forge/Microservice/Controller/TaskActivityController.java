bb6e32d34db7f79b36cf07c134fb7181961de71b
Kshitij prajapati <prajapatikshitij566@gmail.com>
2025-03-31 20:47:18 +0530

    Task Updated and Due Soon Funtionality added

---
+++ Microservice/src/main/java/com/Task_Forge/Microservice/Controller/TaskActivityController.java
@@ -0,0 +1,28 @@
+package com.Task_Forge.Microservice.Controller;
+
+import com.Task_Forge.Microservice.Entity.TaskActivity;
+import com.Task_Forge.Microservice.Service.TaskServiceActivity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/task-activities")
+public class TaskActivityController {
+
+    @Autowired
+    TaskServiceActivity taskServiceActivity;
+
+    @GetMapping("/stats")
+    public Map<String, Long> getUpdatesInLast7Days(){
+        long updatesInLast7Days = taskServiceActivity.getUpdatesInLast7Days();
+        long dueSoonTasks = taskServiceActivity.getDueSoonTasks();
+
+        return Map.of("updatesLast7Days", updatesInLast7Days,
+                      "dueSoonTasks", dueSoonTasks);
+    }
+}
---
+++ Microservice/src/main/java/com/Task_Forge/Microservice/ENUM/ActivityType.java
@@ -0,0 +1,9 @@
+package com.Task_Forge.Microservice.ENUM;
+
+public enum ActivityType {
+    STATUS_CHANGE,
+    COMMENT,
+    ASSIGNMENT,
+    ATTACHMENT,
+    OTHER
+}
