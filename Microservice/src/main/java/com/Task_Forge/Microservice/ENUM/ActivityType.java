a211871c7e74b62cd4f618cf5c5a6833ee219647
Aman Gupta <amangupta0213@gmail.com>
2025-04-04 20:13:05 +0530

    Changed ENUMS

--- Microservice/src/main/java/com/Task_Forge/Microservice/ENUM/ActivityType.java
+++ Microservice/src/main/java/com/Task_Forge/Microservice/ENUM/ActivityType.java
@@ -1,9 +1,9 @@
 package com.Task_Forge.Microservice.ENUM;
 
 public enum ActivityType {
-    STATUS_CHANGE,
-    COMMENT,
-    ASSIGNMENT,
-    ATTACHMENT,
-    OTHER
+    CREATED,          // Task was created
+    UPDATED,          // Task details were modified
+    STATUS_CHANGED,   // Task status changed (e.g., from OPEN to IN_PROGRESS)
+    COMMENT_ADDED,    // A comment was added to the task
+    PRIORITY_CHANGED  // Task priority was updated
 }
